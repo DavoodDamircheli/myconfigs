5f71514b6aeb3e365c728d021548466157ac67d0
Davood <dd1424@msstate.edu>
2026-03-26 11:09:09 -0500

    ranger is added too

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-echo "Installing packages..."
+echo "Updating system..."
 sudo apt update
-sudo apt install -y vim tmux git curl
 
-echo "Linking configs..."
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+echo "Installing core packages..."
+sudo apt install -y git vim tmux ranger curl wget
 
-echo "Done!"
+echo "Creating config directories..."
+mkdir -p ~/.config
+
+echo "Linking dotfiles..."
+
+# core configs
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# ranger config
+ln -sf ~/dotfiles/ranger ~/.config/ranger
+
+echo "Reloading bash..."
+source ~/.bashrc
+
+echo "Setup complete ✅"
